13ae06566cd39dcec7667ebac30c499c6c4e2ed6
KS Durazo <sdurazo.UNLV@gmail.com>
2023-07-09 11:13:06 -0500

    jazzed up form page

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,6 +7,29 @@ function new_form () {
           <main>
             <h1>Add a New Place</h1>
           </main>
+          <form method="POST" action="/places">
+            <div>
+              <label htmlFor="name">Place Name</label>
+              <input id="name" name="name" required />
+            </div>
+            <div>
+              <label htmlFor="pic">Place Picture</label>
+              <input id="pic" name="pic" />
+            </div>
+            <div>
+              <label htmlFor="city">City</label>
+              <input id="city" name="city" />
+            </div>
+            <div>
+              <label htmlFor="state">State</label>
+              <input id="state" name="state" />
+            </div>
+            <div>
+              <label htmlFor="cuisines">Cuisines</label>
+              <input id="cuisines" name="cuisines" required />
+            </div>
+              <input type="submit" value="Add Place" />
+            </form>
         </Def>
     )
 }
