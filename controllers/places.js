406b53ea3f99cae7e2ce2cc72699f659658301dd
KS Durazo <sdurazo.UNLV@gmail.com>
2023-07-09 11:25:04 -0500

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,12 +1,23 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 router.get('/new', (req, res) => {
