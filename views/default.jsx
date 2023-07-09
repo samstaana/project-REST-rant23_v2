a2db57ad89d5a0d9103586d7e0ba47deb1428580
KS Durazo <sdurazo.UNLV@gmail.com>
2023-07-09 11:35:59 -0500

    navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,19 @@ function Def (html) {
                 {/* Note: You don't need to put the word 'public' in your path even though the folder is called that. Since we told Express what our static folder is called, it already knows to look there! */}
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
